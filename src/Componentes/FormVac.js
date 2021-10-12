f7d59bd4b60e8a53eac6399b57eddb28dae75316
joseleyva <17040457@itdurango.edu.mx>
2021-10-12 15:50:26 -0500

    Update FormVac.js

--- src/Componentes/FormVac.js
+++ src/Componentes/FormVac.js
@@ -1,33 +1,37 @@
 import { Formik } from 'formik'
-import React from 'react'
+import React,{useState} from 'react'
 import * as yup from 'yup';
 import { Form, Col, Button, Row, InputGroup } from 'react-bootstrap';
 
-const schema = yup.object().shape({
-    Nombre: yup.string().required("Ingrese el puesto").matches(/^[a-zA-Z ]+$/, "Solo letras"),
-    NumeroP: yup.number().required("Numero de vacantes").min(1, 'Ingrese un numero'),
-    Actividades: yup.string().max(100, 'Muy largo').min(0, 'Ingrese la descripción').required("Describir la actividad"),
-    PuestoR: yup.string().required("Seleccione el puesto"),
-    Dias: yup.string().required("Ingrese los días laborales").matches(/^[a-zA-Z -&]+$/),
-    Horario: yup.string().required("Ingrese el horario laboral"),
-    Turno: yup.string().required("Ingrese el Turno").matches(/^[a-zA-Z -&]+$/),
-    DiasPago: yup.string().required("Ingrese de pagó").matches(/^[0-9]+$/),
-    Semana: yup.string().required("").matches(/^[0-9]+$/),
-    Viajar: yup.string().required("Seleccione la opcion").matches(/^[a-zA-Z -&]+$/),
-    Lugar: yup.string().required("Ingrese el lugar de trabajo").matches(/^[a-zA-Z -&]+$/),
-    Rango: yup.string().required("Ingrese el rango de edad").matches(/^[a-zA-Z -&]+$/),
-    Sexo: yup.string().required("Seleccione una opcion").matches(/^[a-zA-Z -&]+$/),
-    Discapacidad: yup.string().required("Seleccione una opción").matches(/^[a-zA-Z -&]+$/),
-    GenCar: yup.string().required("Seleccione una opción").matches(/^[a-zA-Z -&]+$/),
 
-});
 const FormVac = () => {
+    const [ setEnviado] = useState(false);
+
+    const schema = yup.object().shape({
+        Nombre: yup.string().required("Ingrese el puesto").matches(/^[a-zA-Z ]+$/, "Solo letras").min(4, 'Muy corto'),
+        NumeroP: yup.number().required("Numero de vacantes").min(1, 'Ingrese un numero'),
+        Actividades: yup.string().max(100, 'Muy largo').min(0, 'Ingrese la descripción').required("Describir la actividad"),
+        PuestoR: yup.string().required("Seleccione el puesto"),
+        Dias: yup.string().required("Ingrese los días laborales").matches(/^[a-zA-Z -&]+$/),
+        Horario: yup.string().required("Ingrese el horario laboral"),
+        Turno: yup.string().required("Ingrese el Turno").matches(/^[a-zA-Z -&]+$/),
+        DiasPago: yup.string().required("Ingrese de pagó").matches(/^[0-9]+$/),
+        Semana: yup.string().required("").matches(/^[0-9]+$/),
+        Viajar: yup.string().required("Seleccione la opcion").matches(/^[a-zA-Z -&]+$/),
+        Lugar: yup.string().required("Ingrese el lugar de trabajo").matches(/^[a-zA-Z -&]+$/),
+        Rango: yup.string().required("Ingrese el rango de edad").matches(/^[a-zA-Z -&]+$/),
+        Sexo: yup.string().required("Seleccione una opcion").matches(/^[a-zA-Z -&]+$/),
+        Discapacidad: yup.string().required("Seleccione una opción").matches(/^[a-zA-Z -&]+$/),
+        GenCar: yup.string().required("Seleccione una opción").matches(/^[a-zA-Z -&]+$/),
+    
+    });
+
+
     return (
         <div className="VacanteForm">
         
             <Formik
-                validationSchema={schema}
-                onSubmit={console.log}
+                
                 initialValues={{
                     Nombre: "",
                     NumeroP: "",
@@ -45,17 +49,18 @@ const FormVac = () => {
                     Discapacidad: "",
                     GenCar: "",
                 }}
-            >
+                onSubmit={(valores, {resetForm})=>{
+                    console.log(valores)
+                    setEnviado(true);
+                    setTimeout(()=>setEnviado(false),5000);
+                }}
+                validationSchema={schema}
+           >
                 {({
-                    handleSubmit,
-                    handleChange,
-                    handleBlur,
-                    values,
                     touched,
-                    isValid,
                     errors,
                 }) => (
-                    <Form noValidate onSubmit={handleSubmit} className="FormDatos">
+                    <Form className="FormDatos">
                         <h4>Datos de la vacante</h4>
                         <Row className="mb-3">
                             <Form.Group as={Col} md="4" controlId="validationFormik01" className="position-relative">
@@ -63,10 +68,8 @@ const FormVac = () => {
                                 <Form.Control
                                     type="text"
                                     name="Nombre"
-                                    value={values.Nombre}
-                                    onChange={handleChange}
-                                    isValid={touched.Nombre && !errors.Nombre}
-                                    isInvalid={!!errors.Nombre}
+                                   errors={errors.Nombre}
+                                   touched={touched.Nombre}
                                 />
                                 <Form.Control.Feedback type="invalid" tooltip>{errors.Nombre}</Form.Control.Feedback>
 
@@ -76,10 +79,9 @@ const FormVac = () => {
                                 <Form.Control
                                     type="number"
                                     name="NumeroP"
-                                    value={values.NumeroP}
-                                    onChange={handleChange}
-                                    isValid={touched.NumeroP && !errors.NumeroP}
-                                    isInvalid={!!errors.NumeroP}
+                                    errors={errors.NumeroP}
+                                    touched={touched.NumeroP}
+                                   
                                 />
                                 <Form.Control.Feedback type="invalid" tooltip>{errors.Apellidos}</Form.Control.Feedback>
                             </Form.Group>
@@ -94,10 +96,9 @@ const FormVac = () => {
                                         type="text"
                                         placeholder="Descripción"
                                         name="Actividades"
-                                        value={values.Actividades}
-                                        onChange={handleChange}
-                                        isValid={touched.Actividades && !errors.Actividades}
-                                        isInvalid={!!errors.Actividades}
+                                        errors={errors.Actividades}
+                                        touched={touched.Actividades}
+                                       
                                     />
                                     <Form.Control.Feedback type="invalid" tooltip>{errors.Actividades}
                                     </Form.Control.Feedback>
@@ -110,10 +111,6 @@ const FormVac = () => {
                                 <Form.Select id="inlineFormCustomSelect"
                                     type="text"
                                     name="PuestoR"
-                                    value={values.PuestoR}
-                                    onChange={handleChange}
-                                    isValid={touched.PuestoR && !errors.PuestoR}
-                                    isInvalid={!!errors.PuestoR}
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -127,10 +124,7 @@ const FormVac = () => {
                                 <Form.Control
                                     type="text"
                                     name="Dias"
-                                    value={values.Dias}
-                                    onChange={handleChange}
-                                    isValid={touched.Dias && !errors.Dias}
-                                    isInvalid={!!errors.Dias}
+                                   
                                 />
                                 <Form.Control.Feedback type="invalid" tooltip>{errors.Dias}</Form.Control.Feedback>
 
@@ -140,10 +134,7 @@ const FormVac = () => {
                                 <Form.Control
                                     type="time"
                                     name="Horario"
-                                    value={values.Horario}
-                                    onChange={handleChange}
-                                    isValid={touched.Horario && !errors.Horario}
-                                    isInvalid={!!errors.Horario}
+                                  
                                 />
                                 <Form.Control.Feedback type="invalid" tooltip>{errors.Horario}</Form.Control.Feedback>
 
@@ -157,10 +148,7 @@ const FormVac = () => {
                                 <Form.Control
                                     type="text"
                                     name="Turno"
-                                    value={values.Turno}
-                                    onChange={handleChange}
-                                    isValid={touched.Turno && !errors.Turno}
-                                    isInvalid={!!errors.Turno}
+                                    
                                 />
 
                                 <Form.Control.Feedback type="invalid" tooltip>{errors.Turno}
@@ -171,10 +159,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="DiasPago"
-                                    value={values.DiasPago}
-                                    onChange={handleChange}
-                                    isValid={touched.DiasPago && !errors.DiasPago}
-                                    isInvalid={!!errors.DiasPago}
+                                    
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -189,10 +174,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="Semana"
-                                    value={values.Semana}
-                                    onChange={handleChange}
-                                    isValid={touched.Semana && !errors.Semana}
-                                    isInvalid={!!errors.Semana}
+                                   
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -210,10 +192,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="Viajar"
-                                    value={values.Viajar}
-                                    onChange={handleChange}
-                                    isValid={touched.Viajar && !errors.Viajar}
-                                    isInvalid={!!errors.Viajar}
+                                   
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -228,10 +207,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="Lugar"
-                                    value={values.Lugar}
-                                    onChange={handleChange}
-                                    isValid={touched.Lugar && !errors.Lugar}
-                                    isInvalid={!!errors.Lugar}
+                                    
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -245,10 +221,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="Rango"
-                                    value={values.Rango}
-                                    onChange={handleChange}
-                                    isValid={touched.Rango && !errors.Rango}
-                                    isInvalid={!!errors.Rango}
+                                   
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -263,10 +236,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="Sexo"
-                                    value={values.Sexo}
-                                    onChange={handleChange}
-                                    isValid={touched.Sexo && !errors.Sexo}
-                                    isInvalid={!!errors.Sexo}
+                                   
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -279,10 +249,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="Discapacidad"
-                                    value={values.Discapacidad}
-                                    onChange={handleChange}
-                                    isValid={touched.Discapacidad && !errors.Discapacidad}
-                                    isInvalid={!!errors.Discapacidad}
+                                   
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>
@@ -295,10 +262,7 @@ const FormVac = () => {
                                 <Form.Select
                                     type="text"
                                     name="GenCar"
-                                    value={values.GenCar}
-                                    onChange={handleChange}
-                                    isValid={touched.GenCar && !errors.GenCar}
-                                    isInvalid={!!errors.GenCar}
+                                   
                                 >
                                     <option value="0"></option>
                                     <option value="1">One</option>

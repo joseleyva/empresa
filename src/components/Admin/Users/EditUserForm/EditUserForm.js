54fec9cc3dfd06466e33852dbe5b07d6e89ea26b
joseleyva <17040457@itdurango.edu.mx>
2021-12-06 17:58:24 -0600

    Update EditForm

--- src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -1,14 +1,31 @@
 import React,{useState, useCallback} from 'react';
-import {Avatar, Form, Input, Select, Button, Row, Col} from 'antd';
+import {Avatar,Select} from 'antd';
 import {useDropzone} from 'react-dropzone';
 import "./EditUserForm.scss";
+import { Form, Col, Button, Row, InputGroup } from 'react-bootstrap';
 import NoAvatar from "../../../../assets/img/png/logo512.png"
-
+import {UserOutlined} from '@ant-design/icons';
+ 
 export default function EditUserForm(props){
-   const [avatar, setAvatar]=useState(null);
+   const {user} = props;
+   const [userData, setUserData]= useState({
+       name: user.name,
+       lastname: user.lastname,
+       email: user.email,
+       role: user.role,
+       active: user.active,
+       avatar: user.avatar
+   });
+   const updateUser= e=>{
+       e.preventDefault();
+       console.log(userData);
+
+   }
+    const [avatar, setAvatar]=useState(null);
     return(
         <div className="edit-user-form">
         <UploadAvatar avatar={avatar} setAvatar={setAvatar}/>
+        <EditForm userData={userData} setUserData={setUserData} updateUser={updateUser}/>
         </div>
     )
 }
@@ -41,4 +58,110 @@ function UploadAvatar(props){
             )}
         </div>
     );
+}
+
+function EditForm(props){
+    const { userData, setUserData, updateUser}=props;
+    const {Option} = Select;
+    return(
+        <Form className="form-edit" onSubmit={updateUser}>
+
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationFormik01" className="position-relative">
+              <Form.Label>Nombre(s)</Form.Label>
+              <Form.Control
+              prefix={<UserOutlined/>}
+                type="text"
+                name="Nombre"
+                placeholder="Nombre(s)"
+                value={userData.name}
+                onChange={e=> setUserData({
+                    ...userData, name: e.target.value
+                })}
+              />
+            </Form.Group>
+            <Form.Group as={Col} md="4" controlId="validationFormik02" className="position-relative">
+              <Form.Label>Apellido Materno</Form.Label>
+              <Form.Control
+                type="text"
+                name="Apellidos"
+                placeholder="Apellidos"
+                value={userData.lastname}
+                onChange={e=> setUserData({
+                    ...userData, lastname: e.target.value
+                })}
+                required
+              />
+            </Form.Group>
+
+          </Row>
+          <Row className="mb-3">
+            <Form.Group as={Col} md="4" controlId="validationFormikUsername" className="position-relative">
+              <Form.Label>Correo</Form.Label>
+              <InputGroup hasValidation>
+                <Form.Control
+                  type="text"
+                  placeholder="Correo"
+                  value={userData.email}
+                  onChange={e=> setUserData({
+                      ...userData, email: e.target.value
+                  })}
+                />
+              </InputGroup>
+            </Form.Group>
+            <Form.Group as={Col} md="6" controlId="validationFormik04" className="position-relative">
+            <Form.Label>Roles</Form.Label>
+            <Form.Select
+                  type="select"
+                 placeholder="Seleccióna un rol"
+                 onChange={e=> setUserData({...userData, role: e.target.value})}
+                 value={userData.role}
+                >
+                  <option value="admin">Administrador</option>
+                  <option value="editor"> Editor</option>
+                  <option value="review">Revisor</option>
+                </Form.Select>
+              </Form.Group>
+          </Row>
+          <Row className="mb-3">
+         
+            <Form.Group as={Col} md="6" controlId="validationFormik04" className="position-relative">
+              <Form.Label>Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                onChange={e=>setUserData({...userData, password: e.target.value})}
+              />
+            </Form.Group>
+            <Form.Group as={Col} md="6" controlId="validationFormik03" className="position-relative">
+            <Form.Label> Repetir Contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Repetir Contraseña"
+                onChange={e=>setUserData({...userData, repeatpassword: e.target.value})}
+              />
+              </Form.Group>
+          </Row>
+
+          <Row className="mb-3">
+          <Form.Group as={Col} md="6" controlId="validationFormik04" className="position-relative">
+            <Form.Label>Activar</Form.Label>
+            <Form.Select
+                  type="select"
+                 placeholder="Seleccióna una opcion"
+                 onChange={e=> setUserData({...userData, active: e.target.value})}
+                 value={userData.active}
+                >
+                  <option value="true">Activo</option>
+                  <option value="false">Inactivo</option>
+                </Form.Select>
+              </Form.Group>
+          </Row>
+          <Form.Group as={Col} md="10">
+              <Button variant="primary" type="submit">Actualizar datos</Button>
+            </Form.Group>
+        </Form>
+
+    )
+
 }
